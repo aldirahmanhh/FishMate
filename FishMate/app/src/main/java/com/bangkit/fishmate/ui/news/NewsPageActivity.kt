d84ffaeb860053fe1915de8dfe853b3f42ae4619
aldirahmanhh <aldirahmanhh@gmail.com>
2024-12-10 18:13:31 +0700

    update make back button

--- FishMate/app/src/main/java/com/bangkit/fishmate/ui/news/NewsPageActivity.kt
+++ FishMate/app/src/main/java/com/bangkit/fishmate/ui/news/NewsPageActivity.kt
@@ -1,13 +1,16 @@
 package com.bangkit.fishmate.ui.news
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.fishmate.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NewsPageActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId", "SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_page)
@@ -21,5 +24,11 @@ class NewsPageActivity : AppCompatActivity() {
         url?.let {
             webView.loadUrl(it)
         }
+
+        val fabBack: FloatingActionButton = findViewById(R.id.fab_back)
+        fabBack.setOnClickListener {
+            finish()
+        }
+
     }
 }
