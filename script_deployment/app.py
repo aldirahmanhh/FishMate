0cbbd7c1abab788b25b39b1b1197efe427a430a2
BAYBAS00 <sebastianbayu01@gmail.com>
2024-12-12 11:54:18 +0700

    cloud-storage

--- script_deployment/app.py
+++ script_deployment/app.py
@@ -7,6 +7,8 @@ from PIL import Image
 from werkzeug.utils import secure_filename
 import io
 from google.cloud import firestore
+from google.cloud import storage
+import time
 
 app = Flask(__name__)
 
@@ -15,6 +17,14 @@ key_file = 'firestore-service-account.json'
 
 # initial firestore credential file
 db = firestore.Client.from_service_account_json(key_file)
+
+# Initialize Google Cloud Storage client
+storage_client = storage.Client()
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./storage-service-account.json"
+
+# Specify Google Cloud Storage bucket name
+bucket_name = 'fishmate-bucket'
+bucket = storage_client.get_bucket(bucket_name)
 
 # Set the folder to store uploaded files and the allowed file types
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -42,18 +52,38 @@ def sapa():
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
+# Function to upload file to Google Cloud Storage with predicted label as filename
+def upload_to_gcs(file, predicted_label):
+    # Get the file extension (e.g., .jpg, .png)
+    file_extension = file.filename.rsplit('.', 1)[1].lower()
+    
+    # Generate a filename based on the predicted label and original file extension
+    filename = f"{secure_filename(predicted_label)}-{int(time.time())}.{file_extension}"
+    # print(f"Generated filename: {filename}")
+    
+    # Create a blob in the specified bucket and upload the file
+    blob = bucket.blob(f'uploads/{filename}')
+    
+    # Upload the file to Cloud Storage
+    blob.upload_from_file(file)
+    
+    # Optionally, make the file publicly accessible
+    # blob.make_public()
+    
+    return blob.public_url  # Return the URL of the uploaded file
+
 # Function to preprocess the image
-def preprocess_image(image_path):
-    with open(image_path, 'rb') as file:
-        image_bytes = file.read()
-        pillow_img = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+def preprocess_image(file):
+    # with open(image_path, 'rb') as file:
+    #     image_bytes = file.read()
+    pillow_img = Image.open(file.stream).convert('RGB')
 
     # Transform image same as for training
     data = np.asarray(pillow_img)
     data = data/255.0
     data = np.expand_dims(data, axis=0)  
-    image = tf.image.resize(data, [224, 224])
-    return image
+    image_resize = tf.image.resize(data, [224, 224])
+    return image_resize
 
 @app.route('/upload', methods=['POST'])
 def upload_and_classify():
@@ -68,21 +98,30 @@ def upload_and_classify():
 
         if file and allowed_file(file.filename):
             # Save the file
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
+            # filename = secure_filename(file.filename)
+            # file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            # file.save(file_path)
+
+            # Reset file stream to the beginning before processing
+            file.stream.seek(0)
 
             # Preprocess the image
-            image = preprocess_image(file_path)
+            image_data = preprocess_image(file)
 
             # Perform prediction
-            predictions = model(image)
+            predictions = model(image_data)
             #predictions = model.predict(image)
             predicted_class = np.argmax(predictions[0]) # Assuming softmax output
             confidence = float(np.max(predictions[0]))
             predictions_numpy = tf.Variable(predictions).numpy().tolist()
             # Get the label from the LABEL array
             predicted_label = LABEL[predicted_class]
+
+            # Reset file stream to the beginning before uploading
+            file.stream.seek(0)
+
+            # Upload the file to Google Cloud Storage using predicted label
+            file_url = upload_to_gcs(file, predicted_label)
 
             # inital path to connect to firestore
             diagnosis_ref = db.collection('diagnosis')
@@ -97,7 +136,7 @@ def upload_and_classify():
             # Return the prediction result
             return jsonify({
                 "message": "File uploaded and classified successfully",
-                "file_path": file_path,
+                "file_url": file_url,
                 "model_output": predictions_numpy,
                 "diagnosis": diagnosis_data[0],
                 "predicted_class": predicted_label,
